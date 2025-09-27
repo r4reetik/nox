cc1fa80cff9bded8144d6bc2ba4c8a6cb1f5c1da
Sushants-Git <sushantsgml@gmail.com>
2025-09-27 18:11:06 +0530

    change

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),
+    nodePolyfills({
+        globals: {
+            Buffer: true,
+            global: true,
+            process: true,
+        },
+    }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
